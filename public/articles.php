81c3942e1cda4c4b14812951b5b5f809a9bea907
Louis MOULINET--PRIMA <moulinet.l03@gmail.com>
2026-09-20 20:22:44 +0200

    updated la veille tetuehuifdeziuhf

--- public/articles.php
+++ public/articles.php
@@ -216,7 +216,11 @@ $articlesData = $veilleData['articles'] ?? [];
             let sidebarCount = 0;
             const microlinkPromises = [];
 
-            veilleData.forEach((article, index) => {
+            const sortedVeilleData = [...veilleData].sort((a, b) => {
+                return (b.priority ?? Infinity) - (a.priority ?? Infinity);
+            });
+
+            sortedVeilleData.forEach((article) => {
                 const link = article.link ? article.link.replace(/\s+/g, '') : null;
 
                 if (!link) {
